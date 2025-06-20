22db437d6249208839c9ab2865bc3b44cedcbc6d
DESKTOP-3A7BJ1J\THINKPAD <nguyentruongthebao226@gmail.com>
2025-06-20 23:40:20 +0700

    test ask

--- app/Services/QdrantService.php
+++ app/Services/QdrantService.php
@@ -20,4 +20,19 @@ class QdrantService
             'json' => ['points' => $points],
         ]);
     }
+
+    public static function search(string $collection, array $vector, int $limit = 3): array
+    {
+        $client = self::client();
+
+        $response = $client->post("/collections/$collection/points/search", [
+            'json' => [
+                'vector' => $vector,
+                'top' => $limit
+            ]
+        ]);
+
+        return json_decode($response->getBody(), true)['result'];
+    }
+
 }
